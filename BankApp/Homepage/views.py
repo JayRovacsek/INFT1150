9cf31ecd61bc9ba3f6669c2ddf587db49de15012
RandomGeneric <jayrovacsek@gmail.com>
2017-10-06 11:57:56 +1100

    Added pages

--- BankApp/Homepage/views.py
+++ BankApp/Homepage/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render_to_response
 from Homepage.models import posts
 
 def index(request):
-	return render_to_response('homepage\index.html')	#Relative templates location added to avoid confusion
+    	return render_to_response('Homepage/index.html')	#Relative templates location added to avoid confusion
+
+def login(request):
+    	return render_to_response('Homepage/login.html')	#Relative templates location added to avoid confusion
+
+def transfer(request):
+	return render_to_response('Homepage/transfer.html')	#Relative templates location added to avoid confusion
