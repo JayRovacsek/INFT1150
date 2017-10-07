474ea27e50a6d38f7d3562881a5156072ecd66f9
RandomGeneric <jayrovacsek@gmail.com>
2017-10-07 20:17:59 +1100

    Update

--- BankApp/homepage/apps.py
+++ BankApp/homepage/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class HomepageConfig(AppConfig):
+class homepageConfig(AppConfig):
     name = 'homepage'
--- BankApp/login/models.py
+++ BankApp/login/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    customer_ID = models.CharField(primary_key=True, max_length=12)
+    customer_description = models.CharField(max_length=1000)
+    accountID = models.ForeignKey(Account.accountID, on_delete=models.CASCADE)
+
+
+class Name(models.Model):
+    customerID = models.ForeignKey(Customer.customer_ID, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    initials = models.CharField(max_length=2)
+
+
+class Login(models.Model):
+    loginID = models.CharField(primary_key=True, max_length=12)
+    customerID = models.ForeignKey(Customer.customer_ID, on_delete=models.CASCADE)
+    password = models.CharField(max_length=100)
+
+
+class Account(models.Model):
+    accountID = models.CharField(primary_key=True, max_length=12)
+    account_BSB = models.ForeignKey(Bank.BSB, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=20)
+    account_balance = models.CharField(max_length=100000000)
+    account_description = models.CharField(max_length=100)
+
+
+class Bank(models.Model):
+    BSB = models.CharField(max_length=6)
+    bank_name = models.CharField(max_length=50)
+    bank_mailing_address = models.CharField(max_length=100)
+
+
+class ContactDetails(models.Model):
+    contactID = models.CharField(primary_key=True,max_length=12)
+    customer_ID = models.ForeignKey(Customer.customer_ID, on_delete=models.CASCADE)
+    email_address = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=12)
+
+
+class Address(models.Model):
+    contactID = models.ForeignKey(ContactDetails.contactID, on_delete=models.CASCADE)
+    street_number = models.CharField(max_length=5)
+    street_name = models.CharField(max_length=50)
+    suburb = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=4)
+    state = models.CharField(max_length=18)
+    country = models.CharField(max_length=50)
+
+
+class Transfer(models.Model):
+    transferID = models.CharField(primary_key=True, max_length=12)
+    accountID = models.ForeignKey(Account.accountID, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=10)
+    transfer_description = models.CharField(max_length=140)
