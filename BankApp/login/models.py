05b644096159ba550e62c7ed30fd27b5b9fd5930
3than <bentley.ethan@gmail.com>
2017-10-11 20:02:19 +1100

    Kinda fixed
    Deleted most entries from models.py for simplicity.
    
    Deleted 0001_initial.py from: login/migrations
    
    Ran a new migrate and no longer getting column length too big error.

--- BankApp/login/models.py
+++ BankApp/login/models.py
@@ -1,58 +1,5 @@
 from django.db import models
 
-
-class ContactDetails(models.Model):
-    contactID = models.CharField(primary_key=True,max_length=12)
-    email_address = models.EmailField(max_length=50)
-    phone_number = models.CharField(max_length=12)
-
-
-class Address(models.Model):
-    contactID = models.ForeignKey(ContactDetails, on_delete=models.CASCADE)
-    street_number = models.CharField(max_length=5)
-    street_name = models.CharField(max_length=50)
-    suburb = models.CharField(max_length=50)
-    postcode = models.CharField(max_length=4)
-    state = models.CharField(max_length=18)
-    country = models.CharField(max_length=50)
-
-
-class Bank(models.Model):
-    BSB = models.CharField(max_length=6)
-    bank_name = models.CharField(max_length=50)
-    bank_mailing_address = models.CharField(max_length=100)
-
-
 class Account(models.Model):
     accountID = models.CharField(primary_key=True, max_length=12)
-    account_BSB = models.ForeignKey(Bank, on_delete=models.CASCADE)
-    account_type = models.CharField(max_length=20)
-    account_balance = models.CharField(max_length=1)
-    account_description = models.CharField(max_length=100)
-
-
-class Customer(models.Model):
-    customer_ID = models.CharField(primary_key=True, max_length=12)
-    contactID = models.ForeignKey(Address, on_delete=models.CASCADE)
-    customer_description = models.CharField(max_length=1000)
-    accountID = models.ForeignKey(Account, on_delete=models.CASCADE)
-
-
-class Name(models.Model):
-    customerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    initials = models.CharField(max_length=2)
-
-
-class Login(models.Model):
-    loginID = models.CharField(primary_key=True, max_length=12)
-    customerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    password = models.CharField(max_length=100)
-
-
-class Transfer(models.Model):
-    transferID = models.CharField(primary_key=True, max_length=12)
-    accountID = models.ForeignKey(Account, on_delete=models.CASCADE)
-    amount = models.CharField(max_length=10)
-    transfer_description = models.CharField(max_length=140)
+    balance = models.CharField(max_length=20)
--- BankApp/login/views.py
+++ BankApp/login/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render_to_response
 
-from login.models import Customer
-from login.models import Name
-from login.models import Login
 from login.models import Account
-from login.models import Bank
-from login.models import ContactDetails
-from login.models import Address
-from login.models import Transfer
 
 def login(request):
     	return render_to_response('login/index.html')	#Relative templates location added to avoid confusion
